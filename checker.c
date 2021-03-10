2f7db52720408c8b384f4122e3a786bcc9c55b43
Flavio Carrara De Capua <flavio@carrara.us>
2021-03-10 14:50:48 -0300

    first practical use ever of ***char

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 20:12:58 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/03/10 13:36:32 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/03/10 14:49:21 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,50 +21,20 @@ int			end_routine(t_stk *stack_a, t_stk *stack_b, int out)
 	return (out);
 }
 
-int			validate_op(char *op)
-{
-	if (ft_check(op, " "))
-		return (0);
-	if (ft_strstr(op, OP_NAMES))
-		return (1);
-	return (0);
-}
-
-int			gnl(t_stk *a, t_stk *b)
-{
-	char	*line;
-	
-	line = 0;
-	while (get_next_line(0, &line))
-	{
-		if (validate_op(line))
-			op_exec(line, &a, &b);
-		else
-		{
-			free(line);
-			return (0);
-		}
-		free(line);
-	}
-	free(line);
-	return (1);
-}
-
 int			main(int argc, char **argv)
 {
 	t_stk	*stack_a;
 	t_stk	*stack_b;
+	char	**ops;
 
-	if (validate_args(argc, argv))
+	ops = 0;
+	if (validate_args(argc, argv) && (validate_stdin_ops(&ops)))
 	{
 		stack_a = init_stack_from_args(argc, argv);
 		stack_b = init_stack_empty();
 		stack_double_log(stack_a, stack_b);
-		stack_log(stack_b);
-		if (gnl(stack_a, stack_b))
-			exit(end_routine(stack_a, stack_b, 0));
-		else
-			exit(end_routine(stack_a, stack_b, 1));
+		ft_strfree2d(ops);
+		exit(end_routine(stack_a, stack_b, 0));
 	}
 	else
 		if (argc != 1)
--- ops.c
+++ ops.c
@@ -6,8 +6,48 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 11:39:18 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/03/10 11:44:36 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/03/10 14:50:17 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "common.h"
+#include "checker.h"
+
+int			validate_op(char *op)
+{
+	if (ft_check(op, " "))
+		return (0);
+	if (ft_strstr(op, OP_NAMES))
+		return (1);
+	return (0);
+}
+
+int			validate_stdin_ops(char ***ops)
+{
+	char	*line;
+	char	*ops_stream;
+
+	ops_stream = 0;
+	line = 0;
+	while (get_next_line(0, &line))
+	{
+		if (!(validate_op(line)))
+		{
+			free(line);
+			free(ops_stream);
+			return (0);
+		}
+		if (ops_stream)
+		{
+			ops_stream = ft_strcatxl(ops_stream, ",");
+			ops_stream = ft_strcatxl(ops_stream, line);
+		}
+		else
+			ops_stream = ft_str(line);
+		free(line);
+	}
+	free(line);
+	printf(">>>%s<<<\n", ops_stream);
+	*ops = ft_split(ops_stream, ',');
+	free(ops_stream);
+	return (1);
+}
